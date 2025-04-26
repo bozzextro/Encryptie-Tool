7ca80a79389f135604c025ecf15a567b05cb6b87
Dervis Acar <12303993@student.pxl.be>
2025-04-26 18:10:57 +0200

    Generate AES/RSA

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace EncryptionTool
 {
@@ -23,6 +24,46 @@ namespace EncryptionTool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GenerateAesButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.GenerateKey();
+
+                string base64Key = Convert.ToBase64String(aes.Key);
+                string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AESKeys");
+                Directory.CreateDirectory(folderPath);
+                string filePath = Path.Combine(folderPath, $"{KeyNameTextBox.Text}.txt");
+
+                File.WriteAllText(filePath, base64Key);
+
+                MessageBox.Show($"AES key succesfully generated and saved to: {filePath}");
+            }
+        }
+
+        private void GenerateRsaButton_Click(object sender, RoutedEventArgs e)
+        {
+            string keyName = KeyNameTextBox.Text;
+
+            using (RSA rsa = RSA.Create(2048))
+            {
+                string privateKey = rsa.ToXmlString(true);
+                string publicKey = rsa.ToXmlString(false);
+
+                string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RSAKeys");
+                Directory.CreateDirectory(folderPath);
+
+                string privateKeyPath = Path.Combine(folderPath, $"{keyName}_private.xml");
+                string publicKeyPath = Path.Combine(folderPath, $"{keyName}_public.xml");
+
+                File.WriteAllText(privateKeyPath, privateKey);
+                File.WriteAllText(publicKeyPath, publicKey);
+
+                MessageBox.Show($"RSA keys succesfully generated and saved to: {publicKeyPath}");
+            }
         }
     }
 }
